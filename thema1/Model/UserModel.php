8704ceaf0236e0a654d9eee05a7e3a3fbe8bb181
dlanaras <70207362+dlanaras@users.noreply.github.com>
2022-12-15 10:35:48 +0100

    refactor: removing autogenerated bs

--- thema1/Model/UserModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class UserModel extends Database
-{
-    public function getUsers($limit)
-    {
-        return $this->select("SELECT * FROM users ORDER BY user_id ASC LIMIT ?", ["i", $limit]);
-    }
-}
